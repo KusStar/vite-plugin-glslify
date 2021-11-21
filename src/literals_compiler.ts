ec2b9601eb7d20f565923290b91ac7d2e7a01f57
Kuss <2360314753@qq.com>
2021-11-21 23:33:32 +0800

    :truck: move: literalsCompiler to src/literals_compiler.ts

---
+++ src/literals_compiler.ts
@@ -0,0 +1,54 @@
+import { generate } from 'astring'
+import { walk } from 'estree-walker'
+import { compile } from 'glslify'
+import MagicString from 'magic-string'
+import { Plugin } from 'vite'
+
+import type { Filter } from './types'
+
+export function literalsCompiler(idFilter: Filter, funcFilter: Filter): Plugin {
+  return {
+    name: 'vite-plugin-glslify:literals',
+    transform(code, id) {
+      if (!idFilter(id)) return undefined
+      if (funcFilter(code)) {
+        const ast = this.parse(code)
+        const s = new MagicString(code)
+
+        const compileAndOverwrite = (node: any, start: number, end: number) => {
+          const target = generate(node)
+          try {
+            const compiled = compile(target.replaceAll('`', ''))
+            s.overwrite(start, end, `\`${compiled}\``)
+          } catch (e) {
+            this.error(e.message)
+          }
+        }
+
+        walk(ast, {
+          enter(node) {
+            if (!node.type) {
+              return
+            }
+            if (node?.type === 'TaggedTemplateExpression') {
+              if (funcFilter(node.tag.name)) {
+                const { start, end } = node
+                compileAndOverwrite(node.quasi, start, end)
+              }
+            } else if (node?.type === 'CallExpression') {
+              if (funcFilter(node.callee.name)) {
+                const { start, end } = node
+                compileAndOverwrite(node.arguments[0], start, end)
+              }
+            }
+          }
+        })
+
+        return {
+          code: s.toString()
+        }
+      }
+      return undefined
+    }
+  }
+}
