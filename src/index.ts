cc91bb8177076eebe404b710ce8af29cd57e6f2a
Kuss <2360314753@qq.com>
2021-11-21 23:34:34 +0800

    :recycle: refactor: integrate with files and literals compiler

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,39 @@
-import { createFilter, FilterPattern } from '@rollup/pluginutils'
-import { generate } from 'astring'
-import { walk } from 'estree-walker'
-import { compile } from 'glslify'
-import MagicString from 'magic-string'
+import { createFilter } from '@rollup/pluginutils'
 import { Plugin } from 'vite'
 
-export interface Options {
-  /**
-   * included files or folder, defaults to [/\.ts$/, /\.js$/]
-   */
-  include?: FilterPattern
-  /**
-   * excluded files or folder, defaults to ['node_modules/**']
-   */
-  exclude?: FilterPattern
-  /**
-   * function calling that should be compiled, defaults to [/glsl/]
-   */
-  funcName?: FilterPattern
-}
+import { filesCompiler } from './files_compiler'
+import { literalsCompiler } from './literals_compiler'
+import { Options } from './types'
+
+export const DEFAULT_EXTENSIONS = [/\.vert$/, /\.frag$/, /\.glsl$/]
 
 export function glslifyCompiler(options: Options = {}) {
-  const filter = createFilter(
-    options.include || [/\.ts$/, /\.js$/],
-    options.exclude || ['node_modules/**']
-  )
+  const plugins: Plugin[] = []
+  const transformFiles = options.transformFiles ?? true
+  const transformLiterals = options.transformLiterals ?? true
 
-  const funcFilter = createFilter(options.funcName || [/glsl/])
-  return <Plugin>{
-    name: 'vite-plugin-glslify',
-    transform(code, id) {
-      if (!filter(id)) return undefined
-      if (funcFilter(code)) {
-        const ast = this.parse(code)
-        const s = new MagicString(code)
-
-        const compileAndOverwrite = (node: any, start: number, end: number) => {
-          const target = generate(node)
-          try {
-            const compiled = compile(target)
-            s.overwrite(start, end, `${compiled}`)
-          } catch (e) {
-            this.error(e)
-          }
-        }
-
-        walk(ast, {
-          enter(node) {
-            if (!node.type) {
-              return
-            }
-            if (node?.type === 'TaggedTemplateExpression') {
-              if (funcFilter(node.tag.name)) {
-                const { start, end } = node
-                compileAndOverwrite(node.quasi, start, end)
-              }
-            } else if (node?.type === 'CallExpression') {
-              if (funcFilter(node.callee.name)) {
-                const { start, end } = node
-                compileAndOverwrite(node.arguments[0], start, end)
-              }
-            }
-          }
-        })
-
-        return {
-          code: s.toString()
-        }
-      }
-      return undefined
-    }
+  if (transformFiles) {
+    const extFilter = createFilter(
+      options.extensions || DEFAULT_EXTENSIONS
+    )
+    plugins.push(
+      filesCompiler(extFilter)
+    )
   }
+
+  if (transformLiterals) {
+    const idFilter = createFilter(
+      options.include || [/\.ts$/, /\.js$/],
+      options.exclude || ['node_modules/**']
+    )
+    const funcFilter = createFilter(options.funcName || [/glsl/])
+
+    plugins.push(
+      literalsCompiler(idFilter, funcFilter)
+    )
+  }
+
+  return plugins
 }
 
 export default glslifyCompiler
---
+++ src/types.ts
@@ -0,0 +1,30 @@
+
+import { createFilter, FilterPattern } from '@rollup/pluginutils'
+export type Filter = ReturnType<typeof createFilter>
+
+export interface Options {
+  /**
+   * included files or folder, defaults to [/\.ts$/, /\.js$/]
+   */
+  include?: FilterPattern
+  /**
+   * excluded files or folder, defaults to ['node_modules/**']
+   */
+  exclude?: FilterPattern
+  /**
+   * should transform files with literalsCompiler, defaults to true
+   */
+  transformLiterals?: boolean
+  /**
+   * function calling that should be compiled, defaults to [/glsl/]
+   */
+  funcName?: FilterPattern
+  /**
+   * should transform files with filesCompiler, defaults to true
+   */
+  transformFiles?: boolean
+  /**
+   * extensions of files that should be compiled, defaults to [/\.vert$/, /\.frag$/, /\.glsl$/]
+   */
+  extensions?: FilterPattern
+}
